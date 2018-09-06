75b423ef617553ea07d54a2fc5af79d40d5c163d
Gerardo LSJ <gerardolsj@gmail.com>
2018-09-06 14:03:08 -0500

    init

---
+++ ejercicio1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   int mas = 5;
+   mas++;
+   int menos = 5;
+   menos--;
+
+   return 0;
+}
